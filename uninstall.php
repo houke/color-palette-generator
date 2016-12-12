8e4cacd7f4a8c18181be56b4356d67a21c4e0683
Houke de Kwant <houke@ivaldi.nl>
2016-12-12 21:24:41 +0100

    fix delete

--- uninstall.php
+++ uninstall.php
@@ -2,9 +2,13 @@
 	if (!defined('WP_UNINSTALL_PLUGIN')) {
 	    die;
 	}
+	function delete_custom_terms( $taxonomies ){
+	    global $wpdb;
 
-	// Delete all taxonomies
-	foreach ( array( 'cpg_dominant_color', 'cpg_palette' ) as $taxonomy ) {
-		$wpdb->get_results( $wpdb->prepare( "DELETE t.*, tt.* FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN ('%s')", $taxonomy ) );
-		$wpdb->delete( $wpdb->term_taxonomy, array( 'taxonomy' => $taxonomy ), array( '%s' ) );
+	    foreach ( $taxonomies as $taxonomy ) {
+			$wpdb->get_results( $wpdb->prepare( "DELETE t.*, tt.* FROM $wpdb->terms AS t INNER JOIN $wpdb->term_taxonomy AS tt ON t.term_id = tt.term_id WHERE tt.taxonomy IN ('%s')", $taxonomy ) );
+			$wpdb->delete( $wpdb->term_taxonomy, array( 'taxonomy' => $taxonomy ), array( '%s' ) );
+		}
 	}
+	delete_custom_terms( array( 'cpg_dominant_color', 'cpg_palette' ) );
+
